08645d28eaa43e67c590d5f766fbe31d386e9dde
Tomas Miranda <tomasmiranda98@hotmail.com>
2023-12-14 22:33:29 -0300

    feat:commit initial

--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,21 @@
 import express from 'express';
+
+import typesPokemon from './routes/typesPokemon';
+
+
 const app = express();
 app.use(express.json());//para recibir json
 
 const PORT = 3000;
 /* _par asi ignora el parametro, esto es propio de typescript */
-app.get('/', (req, res) => {
+app.get('/prueba', (req, res) => {
   console.log("el req",req);  
   console.log("el body del req",req.body);
   console.log('Hello world');
   res.send('Hello world');
 });
+
+app.use('/api/typesPokemon', typesPokemon);
 
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
